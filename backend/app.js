d1970fa37c6a78acd5dea41ba4e1b0ee061f3318
skyler <simpso5@wwu.edu>
2025-06-01 18:45:31 -0700

    meaning

--- backend/app.js
+++ backend/app.js
@@ -4,39 +4,34 @@ require('dotenv').config();
 const router = require('./routes/wordRoutes');
 const cors = require('cors');
 
-// Create Express server
 const app = express();
-
-// set the port
 const port = process.env.PORT || 8000;
 
-
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://dictionary-frontend-ycl1.onrender.com', 'https://skylerdictionary.netlify.app', 'https://683cf5d9572570c67128616d--skylerdictionary.netlify.app'],
+  origin: ['http://localhost:3000', 'https://dictionary-frontend-ycl1.onrender.com', 'https://skylerdictionary.netlify.app'],
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true,
 };
 
-app.use(cors());
-app.options('*', cors(corsOptions)); // Handle preflight requests
-
-// Middleware to parse JSON bodies
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
-
-// Connect to MongoDB database
-mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established successfully');
+app.get('/', (req, res) => {
+  res.send('Hello from backend!');
 });
 
-// Routes
+mongoose.connect(process.env.ATLAS_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection established');
+});
+
 app.use('/words', router);
 
-// Start the server
-app.listen(port, '0.0.0.0', () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
