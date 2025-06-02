73a6d8c1aff45119001f186e6632d6ba41bf4d75
skyler <simpso5@wwu.edu>
2025-06-01 18:20:21 -0700

    Final

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,9 @@ const app = express();
 // set the port
 const port = process.env.PORT || 8000;
 
-// Middleware to parse JSON bodies
-app.use(express.json());
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://dictionary-frontend-ycl1.onrender.com', 'https://skylerdictionary.netlify.app', 'https://683cf5d9572570c67128616d--skylerdictionary.netlify.app'],
+  origin: ['http://localhost:3000', 'https://dictionary-frontend-ycl1.onrender.com', 'https://skylerdictionary.netlify.app/', 'https://683cf5d9572570c67128616d--skylerdictionary.netlify.app'],
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true,
@@ -22,6 +20,11 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); // Handle preflight requests
+
+// Middleware to parse JSON bodies
+
+app.use(express.json());
+
 
 // Connect to MongoDB database
 mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
