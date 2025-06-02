d84e859ce6adcdd7d86f8b9daae00d8250fdd35c
skyler <simpso5@wwu.edu>
2025-06-01 18:40:39 -0700

    a

--- backend/app.js
+++ backend/app.js
@@ -37,6 +37,6 @@ connection.once('open', () => {
 app.use('/words', router);
 
 // Start the server
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on port ${port}`);
 });
